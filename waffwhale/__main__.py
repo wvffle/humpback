802c2b4e98472b53448c7cf207565cb2cc72d907
Kasper Seweryn <github@wvffle.net>
2020-11-15 20:04:55 +0100

    feat: Add Windows support

--- waffwhale/__main__.py
+++ waffwhale/__main__.py
@@ -6,13 +6,16 @@ import sys
 from .main import MainWindow
 
 
+if sys.platform == 'win32' and '--my-next-operating-system-wont-be-windows' not in sys.argv:
+    print('We do not support your OS. Use --my-next-operating-system-wont-be-windows to run waffwhale anyway.')
+    sys.exit(1)
+
 app = QApplication(sys.argv)
 
 # Load QSS
 stream = QFile(dirname(__file__) + '/style/app.qss')
 stream.open(QIODevice.ReadOnly)
 app.setStyleSheet(QTextStream(stream).readAll())
-
 
 window = MainWindow()
 
