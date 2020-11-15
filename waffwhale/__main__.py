c17d7f52f033d894c9f1c860e749325038d36892
Kasper Seweryn <github@wvffle.net>
2020-11-15 19:35:11 +0100

    feat: Polish player controls

--- waffwhale/__main__.py
+++ waffwhale/__main__.py
@@ -1,10 +1,18 @@
 from PyQt5.QtWidgets import QApplication
+from PyQt5.QtCore import QFile, QIODevice, QTextStream
+from os.path import dirname
 import sys
 
 from .main import MainWindow
 
 
 app = QApplication(sys.argv)
+
+# Load QSS
+stream = QFile(dirname(__file__) + '/style/app.qss')
+stream.open(QIODevice.ReadOnly)
+app.setStyleSheet(QTextStream(stream).readAll())
+
 
 window = MainWindow()
 
--- waffwhale/widgets/player_controls.py
+++ waffwhale/widgets/player_controls.py
@@ -1,5 +1,5 @@
-from PyQt5.QtWidgets import QWidget
-from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QWidget, QGraphicsDropShadowEffect
+from PyQt5.QtGui import QIcon, QColor
 from PyQt5.QtCore import QSize
 from os.path import dirname
 from ..ui.player_controls import Ui_PlayerControls
@@ -12,31 +12,49 @@ class PlayerControls(QWidget):
         self.ui = Ui_PlayerControls()
         self.ui.setupUi(self)
 
+        size = self.ui.play.geometry().height() / 2
+
+        # Play/Pause button
         self.play_icon = QIcon(dirname(__file__) + '/icons/play.svg')
         self.pause_icon = QIcon(dirname(__file__) + '/icons/pause.svg')
 
-        size = self.ui.play.geometry().height()
-
         self.ui.play.setIcon(self.play_icon)
-        self.ui.play.setIconSize(QSize(size - 4, size - 4))
+        self.ui.play.setIconSize(QSize(size, size))
 
+        self.play_shadow = QGraphicsDropShadowEffect()
+        self.play_shadow.setYOffset(10)
+        self.play_shadow.setXOffset(0)
+        self.play_shadow.setBlurRadius(15)
+        self.play_shadow.setColor(QColor(0xff, 0xaa, 0x00, 0x4c))
+        self.ui.play.setGraphicsEffect(self.play_shadow)
+
+        # Favourite button
         self.ui.fav.setIcon(QIcon(dirname(__file__) + '/icons/heart.svg'))
-        self.ui.fav.setIconSize(QSize(size / 2, size / 2))
+        self.ui.fav.setIconSize(QSize(size, size))
 
+        # Add to playlist button
         self.ui.playlist.setIcon(QIcon(dirname(__file__) + '/icons/folder-plus.svg'))
-        self.ui.playlist.setIconSize(QSize(size / 2, size / 2))
+        self.ui.playlist.setIconSize(QSize(size, size))
 
+        # Hide this artist button
         self.ui.hide.setIcon(QIcon(dirname(__file__) + '/icons/eye-off.svg'))
-        self.ui.hide.setIconSize(QSize(size / 2, size / 2))
+        self.ui.hide.setIconSize(QSize(size, size))
 
-        self.ui.prev.setIcon(QIcon(dirname(__file__) + '/icons/chevron-left.svg'))
-        self.ui.prev.setIconSize(QSize(size - 12, size - 12))
+        # Previous track button
+        self.ui.prev.setIcon(QIcon(dirname(__file__) + '/icons/skip-back.svg'))
+        self.ui.prev.setIconSize(QSize(size, size))
 
-        self.ui.next.setIcon(QIcon(dirname(__file__) + '/icons/chevron-right.svg'))
-        self.ui.next.setIconSize(QSize(size - 12, size - 12))
+        # Next track button
+        self.ui.next.setIcon(QIcon(dirname(__file__) + '/icons/skip-forward.svg'))
+        self.ui.next.setIconSize(QSize(size, size))
 
+        # Mute button
         self.ui.mute.setIcon(QIcon(dirname(__file__) + '/icons/volume-2.svg'))
-        self.ui.mute.setIconSize(QSize(size / 2, size / 2))
+        self.ui.mute.setIconSize(QSize(size, size))
 
+        # Queue button
         self.ui.queue.setIcon(QIcon(dirname(__file__) + '/icons/list.svg'))
-        self.ui.queue.setIconSize(QSize(size / 2, size / 2))
+        self.ui.queue.setIconSize(QSize(size, size))
+
+        # Volume slider
+        # self.ui.volume
