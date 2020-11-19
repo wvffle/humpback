7e888f5e609d74ae42455a97a5887320f041388a
Kasper Seweryn <github@wvffle.net>
2020-11-19 06:52:14 +0100

    feat: Remove CoverDownloader

--- waffwhale/widgets/main.py
+++ waffwhale/widgets/main.py
@@ -1,21 +1,9 @@
 from PySide2.QtWidgets import QMainWindow, QWidget, QVBoxLayout, QHBoxLayout, QStackedLayout
-from PySide2.QtGui import QPixmap, QIcon, QPalette
-from PySide2.QtCore import QObject, Signal, Slot, QThread, Qt, QSettings
-from PySide2.QtNetwork import QNetworkRequest
-from urllib.request import urlopen, Request
+from PySide2.QtGui import QPalette
+from PySide2.QtCore import Slot, Qt, QSettings
 
 from waffwhale.widgets import Sidebar, PlayerControls, Browse, HistoryEntry
 from ..api import API
-
-
-# https://stackoverflow.com/a/59537535/5565538
-class CoverDownloader(QObject):
-    resultsChanged = Signal(bytes)
-
-    @Slot(str)
-    def download(self, url):
-        img = urlopen(url).read()
-        self.resultsChanged.emit(img)
 
 
 class MainWindow(QMainWindow):
@@ -26,13 +14,6 @@ class MainWindow(QMainWindow):
         self.api.history.all().connect(self.on_history)
 
         self.setWindowTitle('waffwhale')
-
-        self.downloader_thread = QThread(self)
-        self.downloader_thread.start()
-
-        self.downloader = CoverDownloader()
-        self.downloader.moveToThread(self.downloader_thread)
-        self.downloader.resultsChanged.connect(self.on_download)
 
         self.mainLayout = QVBoxLayout()
         self.mainLayout.setContentsMargins(0, 0, 0, 0)
@@ -67,22 +48,8 @@ class MainWindow(QMainWindow):
         self.centralWidget.setLayout(self.mainLayout)
         self.setCentralWidget(self.centralWidget)
 
-    @Slot(bytes)
-    def on_download(self, img):
-        pixmap = QPixmap()
-        pixmap.loadFromData(img)
-
-        size = self.controls.ui.cover_art.geometry().height()
-        icon = QIcon(pixmap.scaled(size, size, Qt.KeepAspectRatio, Qt.SmoothTransformation))
-        self.controls.set_cover_art(icon)
-
     @Slot(dict)
     def on_history(self, history):
         for entry in history['results']:
             history_entry = HistoryEntry(entry, self.api)
             self.pages.get('exp_browse').ui.history.addWidget(history_entry)
-
-    def closeEvent(self, event):
-        self.downloader_thread.quit()
-        self.downloader_thread.wait()
-        super().closeEvent(event)
